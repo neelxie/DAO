dd9bcf970bd3279fe2f6d46a754f5b0f8d835784
neelxie <kidricederek@gmail.com>
2022-06-02 22:06:26 +0300

    Set up dao

---
+++ hard-hat/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xAB6A0d1CdB8d2d1733893C05C7CcC9FF3De746BE";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
